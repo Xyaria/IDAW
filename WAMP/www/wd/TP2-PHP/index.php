267c5e8cf8802dbdf679252ced60623edbb30eab
Xyaria <seiroxnoudi@gmail.com>
2023-03-13 17:26:15 +0100

    Début TP2 php

---
+++ WAMP/www/wd/TP2-PHP/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta content="text/html; charset=utf-8" http-equiv="Content-type">
+    <title>Première plongée dans </title>
+    <!--<link href="path/to/css" type="text/css" rel="stylesheet">
+    <script src="path/to/script"></script>-->
+</head>
+<body>
+    <div>
+        <?php
+            $nom="bili";
+            echo "Bonjour ", $nom;
+        ?>
+    </div>
+
+
+
+</body>
+</html>
