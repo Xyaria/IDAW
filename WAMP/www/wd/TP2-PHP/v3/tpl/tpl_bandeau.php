70eea6d9bd0c6467ed0f13aace97eda9af55945e
Xyaria <seiroxnoudi@gmail.com>
2023-03-15 11:49:00 +0100

    v3 - Placeholders + petits ajustements

--- WAMP/www/wd/TP2-PHP/v3/tpl/tpl_bandeau.php
+++ WAMP/www/wd/TP2-PHP/v3/tpl/tpl_bandeau.php
@@ -1,7 +1,7 @@
 <?php
     function getHeader(string $currentPage, $language){
-        $format_fr = ['accueil' => 'Accueil', 'cv' => 'Mon Cv', 'projets' => 'Projets', 'contact' => 'Me contacter'];
-        $format_en = ['accueil' => 'Home', 'cv' => 'My Cv', 'projets' => 'Projects', 'contact' => 'Contact Me'];
+        $format_fr = ['accueil' => 'Accueil', 'cv' => 'Mon CV', 'projets' => 'Projets', 'contact' => 'Me contacter'];
+        $format_en = ['accueil' => 'Home', 'cv' => 'My CV', 'projets' => 'Projects', 'contact' => 'Contact Me'];
 
         //$format = ['fr' => $format_fr, 'en' => $format_en];
         $format = ${'format_' .$language};
