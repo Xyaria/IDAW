92669cc00609184fbfa7250367965d67f3e01f57
Milnaye <corentin.candaes@etu.imt-nord-europe.fr>
2023-04-03 14:35:08 +0200

    api user v1 testée

--- WAMP/www/wd/Projet/backend/api/users.php
+++ WAMP/www/wd/Projet/backend/api/users.php
@@ -3,10 +3,10 @@
 POST /root/users
 avec les infos qu'il faut : ajouter
 
-PUT /root/users/{id}
+PUT /root/users
 modifier l'user avce les infos qu'il faut
 
-DELETE /root/users/{id}
+DELETE /root/users
 supprimer
 
 POST /root/users/connect
@@ -15,13 +15,12 @@ connection
 
 /*GLOBAL TODO
 * MANDATORY :
-* Check for duplicate printing of errors
 * Return for new user account : change to logged in ? -> give back id
-* Replace put and delete methods because can't use in-url variables
 *
 * OPTIONAL : 
 * Secure sport level change (if sport level id selected exist)
 * Generaly secure values (check variable length for example)
+* try/catch sur les requettes sql
 *
 * INFORMATIONS :
 * Add JSON_UNESCAPED_UNICODE as 2nd argument of json_encode to get full support of UTF-8
@@ -30,7 +29,6 @@ connection
 */
     require_once("generalAPI.php");
 
-    //TO TEST //TODO error throw
     function main(){        
         if($_SERVER['REQUEST_METHOD'] == 'POST'){
             if(isset($_SERVER['PATH_INFO'])){
@@ -49,143 +47,157 @@ connection
             return;
         }        
         else{
-            //Existe pas
+            jsonMessage(400, "Bad request");
             return;
         }
     }
 
-    //OK
+    function requestNewUser(){
+        $requiredValues= getUserTableColumns();
+
+        $user = json_decode(file_get_contents("php://input"), true);
+        $user = $user[0];
+
+        if(isMissingRequiredValues($user, $requiredValues)){
+            jsonMessage(400, "Missing values");
+            return;
+        }
+        if(doesUserExist($user['login'])){
+            jsonMessage(400, "User with this login already exist");
+            return;
+        }
+
+        //can execute request
+        executeSQLRequest("INSERT INTO utilisateur (login, mdp, nom, prenom, mail, id_niveau, sexe, date_naissance)".
+                            " VALUES ('".$user['login']."', '".$user['mdp']."', '".$user['nom']."', '".$user['prenom']."', '".
+                                        $user['mail']."', ".$user['id_niveau'].", '".$user['sexe']."', '".$user['date_naissance']."')");
+
+        $id = executeSQLRequest("SELECT id_user from utilisateur where login = '" .$user['login']. "'");
+
+        $id = $id[0]['id_user'];
+        $json = ["location" => $id]; 
+        jsonMessage(201, "User succesfully created", $json);
+        
+    }
+
     function requestConnect(){
         $URI = $_SERVER["PATH_INFO"];
         if ($URI != "/connect"){
-            //erreur bad path
+            jsonMessage(400, "Bad path");
             return;
         }
 
         $requiredValues = array('login', 'mdp');
         $user = json_decode(file_get_contents("php://input"), true);
+        $user = $user[0];
         if(isMissingRequiredValues($user, $requiredValues)){
+            jsonMessage(400, "Missing values");
             return;
         }
 
+        //can execute request
         tryLogin($user['login'], $user['mdp']);
     }
 
-    //TO TEST
-    function requestNewUser(){
-        $requiredValues= getUserTableColumns();
-
-        $user = json_decode(file_get_contents("php://input"), true);
-        if(isMissingRequiredValues($user, $requiredValues)){
-            return;
-        }
-
-        if(userExist($user['login'])){
-            //erreur
-            return;
-        }
-
-        //si arrivé ici, c'est que ok
-        //gérer la création de compte
-        //$dbUser = executeSQLRequest("INSERT INTO utilisateur (login, mdp, nom, prenom, mail, niveau, sexe, naissance)
-        //                                VALUES ($user['login'], $user['mdp'], $user['nom'], $user['prenom'], 
-        //                                        $user['mail'], $user['niveau'], $user['sexe'], $user['naissance'])");
-
-        if($dbUser == null){
-            //Error quelque part
-            return;
-        }
-
-        $json = ["location" => $dbUser['id']]; 
-        jsonMessage(201, "User succesfully created", $json);
-        
-    }
-
-    //TO TEST //TODO update user and send back ok
     function requestUpdate(){
         $requiredValues= getUserTableColumns();
         $user = json_decode(file_get_contents("php://input"), true);
-
-
-        /*
-        if(!isIdValide()){
+        $user = $user[0];
+        
+        if(!isIdValide($user)){()
             return;
         }
-        if(countNonMissingRequiredValues() == 0){
-            //error no change
-            return;
-        }
-*/
-        $sqlRequest = "UPDATE TABLE utilisateur SET ";
-        foreach($user[0] as $field => $value){
-            $sqlRequest .= $field ." = ". $value;
-        }
-
-        print_r($_PUT);
-        $sqlRequest .= "WHERE id = ".$_PUT['id'];
-
-        print($sqlRequest);
-        $update = executeSQLRequest($sqlRequest);
-        print_r($update);
-    }
-
-    //TO TEST //TODO remove user and send back ok
-    function requestDeleteUser(){
-        if(!isIdValide()){
+        if(countNonMissingRequiredValues($user, $requiredValues) == 0){
+            jsonMessage(400, "No values updated");
             return;
         }
         
-        //request remove user
+        //can execute request
+        $sqlRequest = "UPDATE utilisateur SET ";
+        foreach($user as $field => $value){
+            if($field != 'id_user'){
+                $sqlRequest .= $field ." = '". $value . "', ";
+            }
+        }
+        $sqlRequest = substr($sqlRequest, 0, strlen($sqlRequest)-2);
+        $sqlRequest .= " WHERE id_user = " . $user['id_user'];
+
+        executeSQLRequest($sqlRequest);
+        jsonMessage(200, "User succesfully updated");
     }
 
-    //TODO check if returned user is ok and send back ok
+    function requestDeleteUser(){
+        $user = json_decode(file_get_contents("php://input"), true);
+        $user = $user[0];
+        if(!isIdValide($user)){ //error message already treated
+            return;
+        }
+
+        //can execute request
+        executeSQLRequest("DELETE FROM utilisateur WHERE id_user = '" .$user['id_user']. "'");
+        jsonMessage(200, "User succesfully deleted");
+        
+    }
+
     function tryLogin($login, $mdp){
         global $pdo;
-        $user = executeSQLRequest("SELECT * FROM `utilisateur` WHERE `login` = ". $login . " AND `mdp` =" . $mdp );
-        print_r($user);
+        $user = executeSQLRequest("SELECT * FROM `utilisateur` WHERE `login` = '". $login . "' AND `mdp` = '" . $mdp ."'");
+
+        if($user == null){
+            jsonMessage(400, "Login or password invalid");
+            return;
+        }
+
+        $json = ["user" => $user[0]]; 
+        jsonMessage(200, "Connected", $json);
         return;
     }
 
-    //TODO error throw
     function isMissingRequiredValues($arrayToCheck, $requiredValues){
         if(countNonMissingRequiredValues($arrayToCheck, $requiredValues) != sizeof($requiredValues)){
-            return false;
+            return true;
         }
-        return true;
+        return false;
     }
 
-    //TO TEST
     function countNonMissingRequiredValues($arrayToCheck, $requiredValues){
         $nonMissingValues = 0;
         foreach($requiredValues as $value){
-            if(!isset($arrayToCheck[$value])){
+            if(isset($arrayToCheck[$value])){
                 $nonMissingValues += 1;
             }
         }
         return $nonMissingValues;
     }
 
-    //TO TEST  //TODO error throw
-    function isIdValide(){
-        if(!isset($_POST['id'])){
-            //erreur id
+    function isIdValide($user){
+
+        if(!isset($user['id_user'])){
+            jsonMessage(400, "Missing Id");
             return false;
         }
-        $id = $_POST['id'];
+        $id = $user['id_user'];
         if(!is_numeric($id)){   //is_numérique renvoie false si chaine nulle
-            //erreur id
+            jsonMessage(400, "Id is invalid");
             return false;
         }
         if(!doesIdExist($id)){
-            //erreur id
+            jsonMessage(400, "User does not exist");
             return false;
         }
         return true;
     }
 
-    //TO TEST
+    function doesUserExist($login){
+        return doesUserIdExist($login, "login");
+    }
+
     function doesIdExist($id){
-        $correspondingUser = executeSQLRequest("SELECT id FROM utilisateur WHERE id = " . $id);
+        return doesUserIdExist($id, "id_user");
+    }
+
+    function doesUserIdExist($value, $field){
+        $correspondingUser = executeSQLRequest("SELECT id_user FROM utilisateur WHERE " .$field. " = '" . $value. "'");
         if($correspondingUser == null){
             return false;
         }
@@ -193,7 +205,7 @@ connection
     }
 
     function getUserTableColumns(){
-        return array('login', 'mdp', 'nom', 'prenom', 'mail', 'niveau', 'sexe', 'naissance');
+        return array('login', 'mdp', 'nom', 'prenom', 'mail', 'id_niveau', 'sexe', 'date_naissance');
     }
 
     main();
