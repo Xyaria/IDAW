4e6d6eb8d72936e558f8673621e2c7df0234f486
Xyaria <seiroxnoudi@gmail.com>
2023-04-04 18:14:45 +0200

    Ajout du reste du CRUD dans l'API Aliment

--- WAMP/www/wd/Projet/backend/api/aliments.php
+++ WAMP/www/wd/Projet/backend/api/aliments.php
@@ -1,16 +1,11 @@
 <?php
-    require_once("../generalAPI.php");
+    require_once("./generalAPI.php");
 
-    function chooseAliments(){
+    function chooseGetAliment(){
         if(isset($_GET['id'])){
             $id = $_GET['id'];
             $result = getAliment($id);
-        }
-        else if(isset($_GET['from']) AND isset($_GET['nb'])){
-            $id = $_GET['from'];
-            $qtt = $_GET['nb'];
-            $result = getXAliments($id, $qtt);
-        }
+        }      
         else if(substr($_SERVER['REQUEST_URI'], -9) !== "aliments/" AND substr($_SERVER['REQUEST_URI'], -8) !== "aliments"){
             jsonMessage(400, "This query is not supported");
             return;
@@ -55,34 +50,107 @@
         return $aliment;
     }
 
-    function getXAliments($id, $qtt){
-        if(!is_numeric($id)){
-            jsonMessage(400, "ID should be numeric");
-            return;
-        }
-        if(!is_numeric($qtt)){
-            jsonMessage(400, "Quantity should be numeric");
-            return;
-        }
-        
-        $aliment = [];
-        for($i = $id; $i < $id+$qtt; $i++){
-            array_push($aliment, getAliment($i));
-        }
-
-        return $aliment;
-    }
-
     function getAllAliments(){
         $aliment = executeSQLRequest("SELECT aliment.LABEL 'Aliment' FROM `aliment`");
         return $aliment;
+    }
+
+    function addAliment(){
+        $data = json_decode(file_get_contents('php://input'), true);
+        
+        $data['type'] = $data['type'] ?? 248;
+        if(!isset($data['label'])){
+            jsonMessage(400, "Missing label");
+            return;
+        }
+
+        if(!is_numeric($data['type'])){
+            jsonMessage(400, "Type should be a numeric ID");
+            return;
+        }
+
+        $typeExist = executeSQLRequest("SELECT ID_TYPE FROM `type` WHERE ID_TYPE = ".$data['type']);
+        if($typeExist == NULL){
+            jsonMessage(404, "This type does not exist");
+            return;
+        }
+
+        $result = executeSQLRequest("INSERT INTO `aliment` (LABEL, TYPE) VALUES ('".$data['label']."', ".$data['type']." )");
+        $db_aliment = executeSQLRequest("SELECT aliment.ID_ALIMENT FROM `aliment` WHERE LABEL = '" .$data['label']. "' AND TYPE = '" .$data['type']. "'");
+        jsonMessage(201, "Aliment has been created", ["Location" => $db_aliment]);
+    }
+
+    function updateAliment(){
+        $data = json_decode(file_get_contents('php://input'), true);
+
+        if(!isset($_GET['id']) OR !is_numeric($_GET['id'])){
+            jsonMessage(400, "ID should be numeric");
+        }
+        $id  = $_GET['id'];
+
+        $db_id = executeSQLRequest("SELECT ID_ALIMENT FROM `aliment` WHERE ID_ALIMENT = $id");
+        if($db_id == NULL){
+            jsonMessage(404, "ID does not exist");
+        }
+
+        if(!isset($data['label']) AND !isset($data['type'])){
+            jsonMessage(400, "Missing parameter");
+            return;
+        }
+
+        if(!is_numeric($data['type'])){
+            jsonMessage(400, "Type should be a numeric ID");
+            return;
+        }
+
+        $db_type = executeSQLRequest("SELECT ID_TYPE FROM `type` WHERE ID_TYPE = ".$data['type']);
+        if($db_type == NULL){
+            jsonMessage(404, "This type does not exist");
+            return;
+        }
+
+        if(isset($data['label'])){
+            $result = executeSQLRequest("UPDATE `aliment` SET `LABEL` = '" .$data['label']. "' WHERE aliment.ID_ALIMENT = $id");
+        }
+        if(isset($data['type'])){
+            $result = executeSQLRequest("UPDATE `aliment` SET `TYPE` = '" .$data['type']. "' WHERE aliment.ID_ALIMENT = $id");
+        }
+
+        $db_aliment = executeSQLRequest("SELECT LABEL, TYPE FROM `aliment` WHERE ID_ALIMENT = $id");
+        jsonMessage(200, "Aliment updated", ["Aliment" => $db_aliment]);
+    }
+
+    function deleteAliment(){
+        $data = json_decode(file_get_contents('php://input'), true);
+
+        if(!isset($_GET['id']) OR !is_numeric($_GET['id'])){
+            jsonMessage(400, "ID should be numeric");
+        }
+        $id  = $_GET['id'];
+
+        $db_id = executeSQLRequest("SELECT ID_ALIMENT FROM `aliment` WHERE ID_ALIMENT = $id");
+        if($db_id == NULL){
+            jsonMessage(404, "ID does not exist");
+        }
+
+        $result = executeSQLRequest("DELETE FROM `aliment` WHERE ID_ALIMENT = $id");
+        jsonMessage(200, "Aliment deleted");
     }
 
     $reqMethod = $_SERVER['REQUEST_METHOD'];
     
     switch($reqMethod){
         case 'GET':
-            chooseAliments();
+            chooseGetAliment();
+            break;
+        case 'POST':
+            addAliment();
+            break;
+        case 'PUT':
+            updateAliment();
+            break;
+        case 'DELETE':
+            deleteAliment();
             break;
         default:
             jsonMessage(405, "Unauthorized Method");
