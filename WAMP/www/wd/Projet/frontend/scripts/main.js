5c385d5a6fccf34388299c33b15e8d2330392297
Xyaria <seiroxnoudi@gmail.com>
2023-04-04 12:13:57 +0200

    Modifications de CSS du dashboard + script basic de gestion de la barre de progrès

--- WAMP/www/wd/Projet/frontend/scripts/main.js
+++ WAMP/www/wd/Projet/frontend/scripts/main.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
-    const cssColor = $(":root").css("--secondary-warm-color-4");
+    const warmColor4 = $(":root").css("--secondary-warm-color-4");
 
     // Paramétrage de DataTable pour la liste des consommations
     $('#aliment').DataTable({
@@ -17,11 +17,11 @@ $(document).ready(function () {
             'Vendredi',
             'Samedi',
             'Dimanche'
-        ],
+        ], // récupérer aurjourd'hui et les 6 dates précédentes (potentiellement garder les jours, où mettre format jj/mm/aaaa)
         datasets: [{
-            label: "grams",
-            data: [300, 50, 100, 150, 79, 230, 18],
-            backgroundColor: cssColor,
+            label: "grammes",
+            data: [300, 50, 100, 150, 79, 230, 18], // à récupérer en fonction de la date d'aujourd'hui
+            backgroundColor: warmColor4,
             hoverOffset: 4
         }]
       };
@@ -40,11 +40,33 @@ $(document).ready(function () {
         }
     });
 
-    // Paramétrage de la barre de progrès (fait maison !)
-        // - Récupérer le ratio de progression de l'ingestion de calorie en fonction des besoins de l'utilisateur
+    // Paramétrage de la barre de progrès
+        // - Récupérer l'ingestion de calorie de l'utilisateur
+        // - Calculer le ratio (ici ?) en fonction de ses besoins journaliers
         // - Modifier la variable dynamiquement :
-    var progress = "50%";
+    
+    var calories = 1800; // à récupérer
+    var caloriesMax = 2500; // à calculer
+    var progress = calories / caloriesMax * 100; // à calculer
+
     const progressBar = $("#bar");
-    $(progressBar).css('width', progress);
+    const calMessage = $("#calories-message");
+    const calCount = $("#calories-count");
+
+    $(progressBar).css('width', progress + "%");
+    $(calCount).text(calories + "/" + caloriesMax + " kcal");
+
+    // changer le message en fonction du progrès (<10 & <50 ?)
+
+    // Message spécial si trop dépassé (à voir où mettre la limite, il y a des recommandations ?)
+    if(progress > 100){
+        $(progressBar).css('width', '100%');
+        $(progressBar).css('background-image', 'linear-gradient(135deg, var(--secondary-cool-color-4) 0%, var(--accent-color) 100%)');
+        $(progressBar).css('border', '3px solid var(--accent-color)');
+
+        $(calMessage).text("Calories max dépassées !!");
+        $(calMessage).css('color', 'var(--accent-color)');
+        $(calCount).css('color', 'var(--accent-color)');
+    }
     
 });
